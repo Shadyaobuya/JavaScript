8113225a93c8c790fce446cab67ecb806bb49ae9
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-03-09 07:22:07 +0300

    added comments

--- assignment1.js
+++ assignment1.js
@@ -1,8 +1,8 @@
-function fruits(){
-    let fruits=["avocado", "grapes","mangoes", "orange", "kiwi"];
-    for(i in fruits){
-        console.log(fruits[i]);
+function fruits(){          //declare a function called fruits
+    let fruits=["avocado", "grapes","mangoes", "orange", "kiwi"];   //store the fruits in an array called fruits
+    for(i in fruits){     //use a for loop to loop through every item in the array
+        console.log(fruits[i]);  //print all the items in the array
 }
 }
-fruits();
+fruits();   //call function fruits
 
